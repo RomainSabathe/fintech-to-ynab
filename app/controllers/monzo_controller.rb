79ce011c9de14b2d1a2bc4a93edb1e174ea68c4b
Romain Sabathe <RSabathe@gmail.com>
2019-11-02 21:54:54 +0000

    Convert amounts to EUR on the fly

--- app/controllers/monzo_controller.rb
+++ app/controllers/monzo_controller.rb
@@ -5,6 +5,15 @@ class MonzoController < ApplicationController
     ynab_budget_id = params[:ynab_budget_id] || ENV['YNAB_BUDGET_ID']
     ynab_account_id = params[:ynab_account_id] || ENV['YNAB_MONZO_ACCOUNT_ID'] || ENV['YNAB_ACCOUNT_ID']
     ynab_client = ::F2ynab::YNAB::Client.new(ENV['YNAB_ACCESS_TOKEN'], ynab_budget_id, ynab_account_id)
+    
+    # Converting the amount to EUR if asked.
+    if ((params[:convert_currency] || ENV.fetch('CONVERT_CURRENCY', false)) && webhook[:data][:local_currency] != "EUR")
+      amount = webhook[:data][:amount]
+      response = Typhoeus.get("https://api.exchangeratesapi.io/latest")
+      local_currency = webhook[:data][:local_currency]
+      rate = JSON.parse(response.response_body)["rates"][local_currency]
+      webhook[:data][:amount] = (amount / rate).round
+    end
 
     import = ::F2ynab::Webhooks::Monzo.new(ynab_client, webhook,
       skip_emoji: ENV['SKIP_EMOJI'].present?,
